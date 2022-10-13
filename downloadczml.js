54d117d0b227d0689b7f26a7539c2d0e55498c8f
user <user@usernoMacBook-Pro.local>
2022-10-13 13:28:59 +0900

    add czml download plugin

--- downloadczml.js
+++ downloadczml.js
@@ -16,16 +16,58 @@ const html = `
     <div id="wrapper">
       <button id="download" type="button">DOWNLOAD CZML FILE</button>
     </div>
-    <script>
-    let marker;
-    let property;
-   
-    window.addEventListener("message", function (e) {
-      if (e.source !== parent) return;
-      console.log(marker,property);
+  <script>
+  let marker;
+  let property;
+ 
+  window.addEventListener("message", function (e) {
+    if (e.source !== parent) return;
 
+    marker = e.data.marker;
+    property = e.data.property;
+    
+    if (property.hasOwnProperty('default') && property.default.modelSize) {
+      let modelSize = property.default.modelSize;
     }
-    </script>
+
+    if (property.hasOwnProperty('default') && property.default.modelUrl) {
+      let modelUrl = property.default.modelUrl;
+    }
+  });
+  
+  document.getElementById("download").addEventListener("click", myFunction);
+
+  function myFunction() {
+    const czml = [ 
+      { 
+        id: "document", 
+        name: "CZML Model", 
+        version: "1.0", 
+      }, 
+      { 
+         id: "aircraft model", 
+         name: "Cesium Air", 
+         position: { 
+            cartographicDegrees: [marker.property.default.location.lat, marker.property.default.location.lng],      
+         }, 
+         model: { 
+          gltf: modelUrl,           
+          scale: modelSize,                                                
+          minimumPixelSize:  128, 
+        }, 
+      }, 
+    ];
+    saveStaticDataToFile(czml);
+
+  }
+  
+
+  function saveStaticDataToFile(data) {
+    var blob = new Blob([data],
+        { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "demo.czml");
+  }
+  </script>
 `
 
       
@@ -36,7 +78,7 @@ const marker = reearth.layers.find(
 );
 
 // Handle data to download
-reearth.on("message", data => {
+reearth.on("message", marker => {
   marker: marker;
   property: reearth.widget.property
 });
