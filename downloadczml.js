75588343d67cb7cf56b6668a938a8fed9965f625
user <user@usernoMacBook-Pro.local>
2022-10-13 13:19:52 +0900

    add czml download plugin

--- downloadczml.js
+++ downloadczml.js
@@ -34,38 +34,6 @@ const html = `
       let modelUrl = property.default.modelUrl;
     }
   }
-  document.getElementById("download").addEventListener("click", myFunction);
-
-  function myFunction() {
-    const czml = [ 
-      { 
-        id: "document", 
-        name: "CZML Model", 
-        version: "1.0", 
-      }, 
-      { 
-         id: "aircraft model", 
-         name: "Cesium Air", 
-         position: { 
-            cartographicDegrees: [marker.property.default.location.lat, marker.property.default.location.lng],      
-         }, 
-         model: { 
-          gltf: modelUrl,           
-          scale: modelSize,                                                
-          minimumPixelSize:  128, 
-        }, 
-      }, 
-    ];
-    saveStaticDataToFile(czml);
-
-  }
-  
-
-  function saveStaticDataToFile(data) {
-    var blob = new Blob([data],
-        { type: "text/plain;charset=utf-8" });
-    saveAs(blob, "demo.czml");
-  }
   </script>
 `
 
